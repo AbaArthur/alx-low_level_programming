fae23a72d1b30d6394aa58842fb268c2d1031071
AbaArthur <abaaidoo@gmail.com>
2023-06-20 16:58:02 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main- Entry Point
+ * Descreption- a function that prints the alphabet, in lowercase, followed by a new line.
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i=97; i<=122; i++)
+	{
+		_putchar(i);
+	}
+	
+	_putchar('\n');
+}
